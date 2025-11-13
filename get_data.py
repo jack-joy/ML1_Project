fa01035f33af140e63f497c3befc5638b8cfe25f
Neel Davuluri <neel.davuluri@gmail.com>
2025-11-13 11:17:36 -0500

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,3 +1,34 @@
+from nba_api.stats.endpoints import LeagueDashPlayerStats
+import time
+from unidecode import unidecode
 import pandas as pd
 
-pd.read_csv("DATA\\2012-2023 NBA Stats.csv")
+#base per-game stats
+stats_base = LeagueDashPlayerStats(
+    season='2024-25',
+    per_mode_detailed='PerGame'
+).get_data_frames()[0]
+time.sleep(1)
+
+#advanced stats
+stats_adv = LeagueDashPlayerStats(
+    season='2024-25',
+    per_mode_detailed='PerGame',
+    measure_type_detailed_defense='Advanced'
+).get_data_frames()[0]
+time.sleep(1)
+
+stats_base['PLAYER_NAME'] = stats_base['PLAYER_NAME'].apply(unidecode)
+stats_adv['PLAYER_NAME'] = stats_adv['PLAYER_NAME'].apply(unidecode)
+nba_df = stats_base.merge(
+    stats_adv,
+    on='PLAYER_ID',
+    suffixes=('_base', '_adv')
+)
+
+#drop duplicates
+if 'PLAYER_NAME_adv' in nba_df.columns:
+    nba_df.drop(columns=['PLAYER_NAME_adv'], inplace=True)
+    nba_df.rename(columns={'PLAYER_NAME_base': 'PLAYER_NAME'}, inplace=True)
+
+nba_df.head()
